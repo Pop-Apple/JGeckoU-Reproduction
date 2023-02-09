c2025da098340874a9ab2dbf6937ba5521e7a289
Pop-Apple <101918076+Pop-Apple@users.noreply.github.com>
2023-02-09 15:32:56 +0900

    Convert UTF16 to Hexadecimal Tool

---
+++ Text-Converter/Text-Converter/Source.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Text_Converter
+{
+    public partial class Source : Form
+    {
+        public Source()
+        {
+            InitializeComponent();
+
+            //TextBox Setting
+            this.ResultTextBox.CharacterCasing = CharacterCasing.Upper;
+            this.ResultTextBox.ReadOnly = true;
+        }
+
+        public string ToHex(string inp)
+        {
+            string outp = string.Empty;
+            char[] value = inp.ToCharArray();
+            foreach (char L in value)
+            {
+                int V = Convert.ToInt32(L);
+                outp += string.Format("00" + "{00:x}", V);
+            }
+            return outp;
+        }
+
+    private void InputTextBox_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (this.InputTextBox.Text.Length == 0)
+                {
+                    this.ResultTextBox.Text = null;
+                }
+                else
+                {
+                    this.ResultTextBox.Text = ToHex(InputTextBox.Text);
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Text Conversion", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+            }
+        }
+
+        private void CopyButton_Click(object sender, EventArgs e)
+        {
+            ResultTextBox.SelectAll();
+            //and
+            ResultTextBox.Copy();
+        }
+
+        private void CancelButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
